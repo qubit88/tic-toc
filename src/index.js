e485aa5e4d93963da94f4b2c0c103c4d867350a6
qubit88 <qubertec@gmail.com>
2019-07-19 20:18:37 +0300

    add previous moves history and starting playing from any past move by overwriting history

--- src/index.js
+++ src/index.js
@@ -53,12 +53,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
-      xIsNext: true
+      xIsNext: true,
+      setNumber: 0
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -66,15 +67,33 @@ class Game extends React.Component {
     }
     squares[i] = this.state.isXNext ? "X" : "O";
     this.setState(prevState => ({
-      history: prevState.history.concat([{ squares }]),
+      history: history.concat([{ squares }]),
+      stepNumber: history.length,
       isXNext: !prevState.isXNext
     }));
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      isXNext: step % 2 === 0
+    });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
 
     if (winner) {
@@ -89,7 +108,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
