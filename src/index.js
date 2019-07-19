b1cdfe8dc2d8950be35191091f554be81f63f345
qubit88 <qubertec@gmail.com>
2019-07-19 19:10:16 +0300

    add click handler for square

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
   render() {
     return (
-      <button onClick={() => this.setState({ value: "X" })} className="square">
-        {this.state.value}
+      <button onClick={() => this.props.onClick()} className="square">
+        {this.props.value}
       </button>
     );
   }
@@ -24,6 +18,12 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null)
     };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares });
   }
   renderSquare(i) {
     return (
