93e53340b6900d10deb56cfd251ba7bc016ed5c6
qubit88 <qubertec@gmail.com>
2019-07-19 19:02:01 +0300

    lift state into Board component, pass down function component

--- src/index.js
+++ src/index.js
@@ -3,18 +3,35 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
     return (
-      <button onClick={() => alert("click")} className="square">
-        {this.props.value}
+      <button onClick={() => this.setState({ value: "X" })} className="square">
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
